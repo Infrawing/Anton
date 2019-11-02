71ede57f456166e4d0118d3c008d1e7b70efa768
Infrawing <infrawing@gmail.com>
2019-11-02 16:01:48 +0530

    init info

---
+++ anton/__init__.py
@@ -0,0 +1,9 @@
+''' Anton twitter bot '''
+
+__author__ = "Chirag"
+__copyright__ = "Copyright 2019, Anton Project"
+__license__ = "MIT"
+___version__ = "1.0.1"
+__maintainer__ = "Chirag"
+__email__ = "infrawing@gmail.com"
+__status__ = "Development"
